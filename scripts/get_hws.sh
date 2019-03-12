35ddf2dbcf510d31d7e3d6f88100a0fee6665399
Anand Dyavanapalli <adyavanapalli@gmail.com>
2019-03-12 23:20:37 +0000

    feat(scripts): Add script for getting hws.

---
+++ scripts/get_hws.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+# get_hws.sh - downloads and extracts all homework files on the OSTEP website.
+
+ALL="all.tgz"
+URL="http://www.cs.wisc.edu/~remzi/OSTEP/Homework/$ALL"
+echo "Attempting to download homework files from: $URL"
+
+# Checking existence of wget.
+command -v wget >/dev/null 2>&1 || { echo >&2 "Missing wget. Aborting."; exit 1; }
+
+# Download and extract `all.tgz` homework file.
+wget "$URL"
+tar zxvf "$ALL" -C homework/
+rm "$ALL"
+
+# Extract every individual homework file.
+cd homework
+for folder in HW-*; do
+    cd "$folder"
+    tar zxvf *.tgz
+    rm -f *.tgz ._*
+    cd ..
+done
+cd ..
