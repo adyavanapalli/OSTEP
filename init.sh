c8601c4b08888d3675fce391238aea5a41f387be
Anand Dyavanapalli <adyavanapalli@gmail.com>
2019-03-13 02:19:43 +0000

    fix(scripts): Add `submodule init` to allow cloning.

--- init.sh
+++ init.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 # init.sh - gets submodules and runs any necessary start-up scripts.
 
+# Get submodules.
+git submodule init
 git submodule update --recursive
+
+# Run start-up scripts.
 ./scripts/get-homework.sh
